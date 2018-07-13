feb02df6bddb2cf4be3e5924d256c1bd4aacf8e2
Mawox <maw00x@gmail.com>
2018-07-14 01:29:39 +0200

    implement circular buffer; rename some variables

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,45 @@
 
 #define PI 3.1415926535897932384626433832795
 
-int WELL_RADIUS = 1000; // in milimeters
+const int WELL_RADIUS = 1000; // in milimeters
 int WATER_LEVEL_MIN = 10000; // in milimeters from sensor
 int WATER_LEVEL_MAX = 20000; // in milimeters from sensor
-int lastWeek[7*24*60] = {}; // array of last weeks worh of data. 
+const int VALUES_TO_KEEP = 7*24*60; 
+int history[VALUES_TO_KEEP]; // array of last weeks worh of data. 
+int circ_index = 0;
 int MINUTE = 60000; //ms
 
 void read_sd_card() {
-    memcpy(lastWeek, lastWeek, sizeof(lastWeek)*sizeof(int) ); //used to load history into memeory
+    memcpy(history, history, sizeof(history)*sizeof(int) ); //used to load history into memeory
 }
 
 int water_volume(int level) {
     int volume = WELL_RADIUS*PI*(WATER_LEVEL_MIN-level);
     return volume;
 }
+int get_value(int* array, int pos){
+    return array[(pos+circ_index)%VALUES_TO_KEEP];
+}
 
-void update_array(int new_value, int* hist_array) {
-    memcpy(hist_array, &hist_array[1], sizeof(hist_array) - sizeof(int));
-    hist_array[sizeof(hist_array)] =  new_value;
+void update_array(int new_value, int* array) {
+    array[circ_index] = new_value;
+    Serial.println(array[circ_index]);
+    circ_index = (circ_index+1)%VALUES_TO_KEEP;
 
-    return;
 }
 
 void measure() {
     //TODO add sensor logic
     int new_water_level = 8;
 
-    update_array(new_water_level, lastWeek);
+    update_array(new_water_level, history);
 
-    return;
 }
 
 void save_data(int* hist_array, int MAX, int MIN) {
     //TODO add save to sd_card
     
-    return;
+    
 }
 
 
@@ -53,16 +57,21 @@ void display_data(int* data) {
 }
 
 void setup() {
-    Serial.begin(57600);
+      // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+
     Serial.println("Started");
     read_sd_card(); // load history into memory after reboot
 }
 
 void loop() {
     Serial.println("Updating...");
-    delay(MINUTE/4);
+    delay(MINUTE/20);
     measure(); // get new data
-    save_data(lastWeek, WATER_LEVEL_MAX, WATER_LEVEL_MIN); // save data in case of a crash
-    display_data(lastWeek); // show data on display
+    save_data(history, WATER_LEVEL_MAX, WATER_LEVEL_MIN); // save data in case of a crash
+    display_data(history); // show data on display
 
 }
